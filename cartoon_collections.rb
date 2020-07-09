e2bde31c6e31059ab03fdd7aaa237a9f4987a66b
adept-bell-4492 <garlandegoma@gmail.com>
2020-07-09 17:00:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index {|greetings_2|
-    puts "1." greetings_2 
+    puts "#{1}. #{greetings_2 }"
   
   }
   
